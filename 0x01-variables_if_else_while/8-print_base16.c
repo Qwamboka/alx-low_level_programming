dfdc788c2dfd81a716ccd25b28f2cb6752777424
Qwamboka <ombatilydia32@gmail.com>
2023-07-16 23:02:45 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#include <time.h>
+
+
+/**
+ * main - Entry point
+ *Return: Always 0 (success)
+ */
+
+
+int main(void)
+        
+{       
+        
+        
+        int d;
+	char low;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+
+}
