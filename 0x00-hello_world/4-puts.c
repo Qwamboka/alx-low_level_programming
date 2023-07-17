d76286852692285ccc38b67a3a60b4bbf9d43b8a
Qwamboka <ombatilydia32@gmail.com>
2023-07-17 12:32:01 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program that printsa line with puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
+
