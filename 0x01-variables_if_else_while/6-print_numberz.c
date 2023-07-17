8aa71e3b45d9a54b96f2b490f90b1dbbfd48efb7
Qwamboka <ombatilydia32@gmail.com>
2023-07-16 22:18:20 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+
+int main(void)
+
+
+{
+	int d;
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	putchar('\n');
+
+	return (0);
+
+
+}
