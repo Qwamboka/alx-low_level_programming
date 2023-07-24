bf32b6946aceed190e160f89fe456defb6b5d2dd
Qwamboka <ombatilydia32@gmail.com>
2023-07-24 15:01:44 -0700

    Task 4

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
 int _putchar(char c);
 
 void reset_to_98(int *n);
@@ -22,3 +28,4 @@ char *_strcpy(char *dest, char *src);
 
 int _atoi(char *s);
 
+#endif
