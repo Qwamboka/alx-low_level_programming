2c9b53ff6b105525ccb11d737c528897e5798934
Qwamboka <ombatilydia32@gmail.com>
2023-07-24 08:01:23 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * swap_int - Swaps the values of two intergers.
+ * @a: The first interger to be swapped.
+ * @b: The second interger to be swapped.
+ *
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+/* the function that swaps the values of two intergers. */
+{
+	int tempo =*a;
+	*a = *b;
+	*b = temp;
+}
+
