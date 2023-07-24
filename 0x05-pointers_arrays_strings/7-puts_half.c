f7ef5ecff12ea67ce2ee83df6dfdfef7042fa20f
Qwamboka <ombatilydia32@gmail.com>
2023-07-24 16:21:28 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * puts_half - prints half a spring
- * @str: The string to print
- * Return: void
+ *puts_half - prints half a spring
+ *@str: The string to print
+ *Return: void
  */
 void puts_half(char *str)
 
@@ -17,13 +17,13 @@ void puts_half(char *str)
 	if (j % 2 == 1)
 	{
 	k = (j - 1) / 2;
-	k+= 1;
+	k += 1;
 	}
 	else
 	{
 	k = j / 2;
 	}
-	 for (; k < j; k++)
+        for (; k < j; k++)
 	{
 	_putchar(str[k]);
 	}
