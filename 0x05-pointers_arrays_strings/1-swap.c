1d8a5515e28d3a3dce9fe1bdd9cf8cf743da6d71
Qwamboka <ombatilydia32@gmail.com>
2023-07-24 08:05:31 -0700

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * swap_int - Swaps the values of two intergers.
- * @a: The first interger to be swapped.
- * @b: The second interger to be swapped.
+ * swap_int - Swaps the values of two integers.
+ * @a: The first integer to be swapped.
+ * @b: The second integer to be swapped.
  *
  * Return: nothing
  */
 
 void swap_int(int *a, int *b)
-/* the function that swaps the values of two intergers. */
+/* the function that swaps the values of two integers. */
 {
 	int tempo =*a;
 	*a = *b;
