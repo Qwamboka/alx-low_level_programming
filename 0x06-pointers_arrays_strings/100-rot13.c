19f4863b905ad5964e8d15784b4fe0fae1e21056
Qwamboka <ombatilydia32@gmail.com>
2023-07-27 12:53:30 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,26 +4,28 @@
 /**
  * rot13 - encoder rot13
  * @s: pointer to string params
+ *
  * Return: *s
  */
+
 char *rot13(char *s)
 {
 int i;
 int j;
-char data[] =
-"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char datarot[] =
-"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
 for (i = 0; s[i] != '\0'; i++)
 {
 for (j = 0; j < 52; j++)
 {
-if (s[1] == data[j])
+if (s[i] == data1[j])
 {
-s[i] = dataroot[j];
+s[i] = datarot[j];
 break;
 }
 }
 }
 return (s);
 }
+
