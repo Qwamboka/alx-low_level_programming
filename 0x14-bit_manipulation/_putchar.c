7d29892231ca7329eae179798f338c3176a87596
Qwamboka <ombatilydia32@gmail.com>
2023-11-02 14:48:12 -0700

    task 1

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to standard output
+ * @c: The character to  be printed
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error number is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
