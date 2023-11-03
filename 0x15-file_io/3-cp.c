403c2991ebc5032b084cf92846cc0420e8d3f765
Qwamboka <ombatilydia32@gmail.com>
2023-11-03 11:38:48 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -101,33 +102,4 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
